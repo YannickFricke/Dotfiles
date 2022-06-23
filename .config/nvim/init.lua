c7f4b51e3e6e31d66f1add4a79042b79c760f7e5
Yannick Fricke <yannickfricke@googlemail.com>
2022-06-23 06:01:00 +0200

    [nvim] Install "packer" on every fresh start

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,17 @@
 utils = require('yannickfricke/utils')
 
 print("Using Yannick Frickes NVIM "..utils.SUPPORTED_VERSION.." configuration")
+
+local packer_bootstrap = utils.InstallPacker()
+
+return require('packer').startup(function(use)
+  -- My plugins here
+  -- use 'foo1/bar1.nvim'
+  -- use 'foo2/bar2.nvim'
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
--- .config/nvim/lua/yannickfricke/utils.lua
+++ .config/nvim/lua/yannickfricke/utils.lua
@@ -2,4 +2,14 @@ local Module = {}
 
 Module.SUPPORTED_VERSION = "0.7.0"
 
+function Module.InstallPacker()
+	local fn = vim.fn
+	local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+	if fn.empty(fn.glob(install_path)) > 0 then
+		return fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+	end
+
+	return false
+end
+
 return Module
